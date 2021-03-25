aab458e23f84b82d1f421543cdc16cf8fb0d8c09
danielpalza <danielpalza@hotmail.com>
2021-03-25 20:12:19 -0300

    Add some comments for Fetch and Edit

--- src/component/Edit.js
+++ src/component/Edit.js
@@ -9,11 +9,13 @@ const Edit = (prop) => {
     userId: prop.post.userId
   });
 
+  //Manage the changes of text input in the state of post
   const controllChange = (p) => {
     setPost({ ...post, [p.target.name]: p.target.value });
   };
   
-
+  //Make the fetch and update with new post data, set it to "data" from Home y return the view
+  //to it
   async function editPost() {
     await Fetch("PUT", prop.post.id, post)
       .then((res) => res.json())
@@ -25,7 +27,7 @@ const Edit = (prop) => {
     prop.setCompRender("HOME");
   }
 
-  //Take the data for make a new post
+  //Take the data for edit a old post
   return (
     <div className="m-3 p-2 shadow-md w-full ">
       <div className="justify-between flex flex-col md:flex-row items-center bg-yellow-500 text-2xl p-5 font-sans">
--- src/services/Fetch.js
+++ src/services/Fetch.js
@@ -1,8 +1,11 @@
-
+//It makes a request to the API according to the way it will be used, 
+//the id (if needed) and / or the body
 async function Fetch(mod, id = "", body = {}) {
   const urlBase = "https://jsonplaceholder.typicode.com/posts";
   let url;
   let myHeaders = new Headers();
+
+  //Config the options for the request
   let myInit =
     mod === "GET"
       ? {
@@ -16,11 +19,14 @@ async function Fetch(mod, id = "", body = {}) {
           mode: "cors",
           body: JSON.stringify(body),
         };
+
+  //Make the url to use      
   mod === "GET" || mod === "POST"
     ? (url = urlBase)
     : (url = `${urlBase}/${id}`);
   let myRequest = new Request(url, myInit);
-  console.log("request:", myRequest)
+
+
   return await fetch(myRequest);
 }
 
